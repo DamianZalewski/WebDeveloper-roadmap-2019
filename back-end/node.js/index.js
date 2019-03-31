1ba5e7d07ff487a1ab4d36e510587a4d3ab610a5
damian_zalewskipl@o2.pl <damian_zalewskipl@o2.pl>
2019-03-31 13:01:11 +0200

    finished node.js tutorial

--- back-end/node.js/index.js
+++ back-end/node.js/index.js
@@ -24,9 +24,31 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.end('<h1>HOM222E</h1>')
-    }
+    // if(req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+    //         res.writeHead(200, {'Content-Type': 'text/html'});
+    //         res.end(data);
+    //     })
+    // }
+    // else if(req.url === '/api/users') {
+    //     const users = [
+    //         { name: 'Bob Smith', age: 40},
+    //         { name: 'John Doe', age: 30}
+    //     ];
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify(users));
+    // }
+
+    // build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    // extension of file
+    let extname = path.extname(filePath);
+
+    // initial content type
+    let contentType = 'text/html';
+
+    // check ext and set content type
 });
 
 const PORT = process.env.PORT || 5000;
